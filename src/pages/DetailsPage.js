a229ae1985cedce2ca55e224649ab74e3015028c
khademishoja <khademishoja@gmail.com>
2022-12-01 10:05:03 +0100

    add bid and set validation

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -16,6 +16,8 @@ const DetailsPage = () => {
   const [amount, setAmount] = useState(0);
   const token = useSelector(selectToken);
   const user = useSelector(selectUser);
+  const [bids, setBid] = useState([]);
+
   const onAmonuntChange = (e) => {
     setAmount(e.target.value);
   };
@@ -25,6 +27,7 @@ const DetailsPage = () => {
 
   useEffect(() => {
     if (artWork) setHearts(artWork.hearts);
+    if (artWork) setBid(artWork.bids);
   }, [artWork]);
   const onheartClick = () => {
     if (artWork) {
@@ -34,14 +37,37 @@ const DetailsPage = () => {
 
     //dispatch(updateHeart(artWork));
   };
+
   const onBidPost = () => {
     if (amount) {
-      dispatch(
-        postBid({ email: user.email, amount: amount, artworkId: artWork.id })
-      );
+      if (bids.length === 0 && artWork.minimumBid >= amount) {
+        alert(`you have to bid more than ${artWork.minimumBid}`);
+      } else {
+        const amounts = bids.map((bid) => {
+          return bid.amount;
+        });
+        const maxAmount = Math.max(...amounts);
+        console.log("max bid", maxAmount);
+        if (amount <= maxAmount) {
+          alert(`you have to bid more than ${maxAmount}`);
+        } else {
+          dispatch(
+            postBid({
+              email: user.email,
+              amount: amount,
+              artworkId: artWork.id,
+            })
+          );
+          setBid([
+            ...bids,
+            { email: user.email, amount: amount, artworkId: artWork.id },
+          ]);
+        }
+      }
     }
   };
-  return artWork && artWork.bids ? (
+
+  return artWork && bids ? (
     <div>
       <div>
         <h2>{artWork.title}</h2>
@@ -49,7 +75,7 @@ const DetailsPage = () => {
         <p>Hearts:{hearts}</p>
         Bids:
         <ul>
-          {artWork.bids.map((item, index) => {
+          {bids.map((item, index) => {
             return (
               <li key={index}>
                 Email:{item.email} <br />
