4d146a5807af022a662b744e10350fcadac1623b
khademishoja <khademishoja@gmail.com>
2022-12-01 14:14:04 +0100

    add checkbox i am artist in signup and change thunks

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,64 +1,78 @@
-import styled from "styled-components"
-import { Button, Input, Title } from "../styled"
-import { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { useNavigate } from "react-router-dom"
-import { signUp } from "../store/user/thunks"
-import { selectToken } from "../store/user/selectors"
+import styled from "styled-components";
+import { Button, Input, Title } from "../styled";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { signUp } from "../store/user/thunks";
+import { selectToken } from "../store/user/selectors";
 
 export const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isArtist, setIsArtist] = useState(false);
 
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-
-  const token = useSelector(selectToken)
+  const token = useSelector(selectToken);
 
   useEffect(() => {
     if (token !== null) {
       navigate("/");
     }
   }, [token, navigate]);
+  // function chState(element) {
+  //   if (element.checked) setIsArtist(true);
+  //   else {
+  //     setIsArtist(false);
+  //   }
+  // }
 
   const submitForm = (e) => {
-    e.preventDefault()
-    dispatch(signUp(name, email, password))
-  }
+    e.preventDefault();
+    dispatch(signUp(name, email, password, isArtist));
+  };
 
   return (
-    <div style={{textAlign: "center"}}>
+    <div style={{ textAlign: "center" }}>
       <Container>
         <Title>Sign Up</Title>
         <form onSubmit={submitForm}>
-          <Input 
+          <Input
             placeholder="name"
-            value={name} 
+            value={name}
             onChange={(e) => setName(e.target.value)}
           />
-          <Input 
+          <Input
             placeholder="email"
-            value={email} 
+            value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
-          <Input 
-            type="password" 
+          <br />
+          <input
+            type="checkbox"
+            value={isArtist}
+            onChange={(e) => setIsArtist(!isArtist)}
+          />
+          I am an artist
+          <br />
+          <Input
+            type="password"
             placeholder="password"
-            value={password} 
+            value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <br/>
+          <br />
           <Button type="submit">Sign Up</Button>
         </form>
       </Container>
     </div>
-  )
-}
+  );
+};
 
 const Container = styled.div`
-  display: 'flex';
-  flex-direction: 'column';
+  display: "flex";
+  flex-direction: "column";
   margin: 15%;
-`
+`;
